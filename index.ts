040e51468fdabaaba39aa401193e03bf64ad98fa
Sunil Srivatsa <sunilsrivatsa@gmail.com>
2021-08-02 20:23:49 -0700

    Add eslint and prettier

--- index.ts
+++ index.ts
@@ -1,25 +1,26 @@
-import {BigNumber} from 'ethers';
-import * as data from './json/retroactive_lp.json'
-import fs from 'fs'
+import * as data from "./json/retroactive_lp.json"
 
-let lpTransferLogs: LpTransferLog[] = data["default"]
+import { BigNumber } from "ethers"
+import fs from "fs"
+
+const lpTransferLogs: LpTransferLog[] = data["default"]
 
 interface LpTransferLog {
-  block_timestamp: string,
-  transaction_hash: string,
-  address_from: string,
-  address_to: string,
-  amount: string,
+  block_timestamp: string
+  transaction_hash: string
+  address_from: string
+  address_to: string
+  amount: string
   pool: string
 }
 
 interface Holders {
   [holder: string]: {
-    lastLPAmountSaved: BigNumber;
-    lastActionTimestamp: number;
-    firstObservedTimestamp: number;
-    timeWightedLPAmount: BigNumber;
-  };
+    lastLPAmountSaved: BigNumber
+    lastActionTimestamp: number
+    firstObservedTimestamp: number
+    timeWightedLPAmount: BigNumber
+  }
 }
 
 interface AllHolders {
@@ -27,31 +28,33 @@ interface AllHolders {
 }
 
 interface TimeWeightedAmountOutput {
-  [pool: string] : {
-    [address: string] : string
+  [pool: string]: {
+    [address: string]: string
   }
 }
 
 interface TimeWeightedAmountOutputByAddress {
-  [address: string] : {
-    [pool: string] : string
+  [address: string]: {
+    [pool: string]: string
   }
 }
 
 function processMinting(holders: Holders, log: LpTransferLog) {
-  let currentTimestamp = Math.round(new Date(log.block_timestamp).getTime() / 1000)
+  const currentTimestamp = Math.round(
+    new Date(log.block_timestamp).getTime() / 1000,
+  )
 
   if (holders.hasOwnProperty(log.address_to)) {
-    let holder = holders[log.address_to]
-    let lastLPAmountSaved = holder.lastLPAmountSaved
-    let timeDiff = currentTimestamp - holder.lastActionTimestamp
-    let timeWeightedLPAmount = lastLPAmountSaved.mul(timeDiff)
+    const holder = holders[log.address_to]
+    const lastLPAmountSaved = holder.lastLPAmountSaved
+    const timeDiff = currentTimestamp - holder.lastActionTimestamp
+    const timeWeightedLPAmount = lastLPAmountSaved.mul(timeDiff)
 
     holders[log.address_to] = {
       lastLPAmountSaved: lastLPAmountSaved.add(log.amount),
       lastActionTimestamp: currentTimestamp,
       firstObservedTimestamp: holder.firstObservedTimestamp,
-      timeWightedLPAmount: holder.timeWightedLPAmount.add(timeWeightedLPAmount)
+      timeWightedLPAmount: holder.timeWightedLPAmount.add(timeWeightedLPAmount),
     }
   } else {
     // No record for log.address_to found.
@@ -60,45 +63,50 @@ function processMinting(holders: Holders, log: LpTransferLog) {
       lastLPAmountSaved: BigNumber.from(log.amount),
       lastActionTimestamp: currentTimestamp,
       firstObservedTimestamp: currentTimestamp,
-      timeWightedLPAmount: BigNumber.from(0)
+      timeWightedLPAmount: BigNumber.from(0),
     }
   }
 }
 
 function processBurning(holders: Holders, log: LpTransferLog) {
-  let currentTimestamp = Math.round(new Date(log.block_timestamp).getTime() / 1000)
+  const currentTimestamp = Math.round(
+    new Date(log.block_timestamp).getTime() / 1000,
+  )
 
-  let holder = holders[log.address_from]
-  let lastLPAmountSaved = holder.lastLPAmountSaved
-  let timeDiff = currentTimestamp - holder.lastActionTimestamp
-  let timeWeightedLPAmount = lastLPAmountSaved.mul(timeDiff)
+  const holder = holders[log.address_from]
+  const lastLPAmountSaved = holder.lastLPAmountSaved
+  const timeDiff = currentTimestamp - holder.lastActionTimestamp
+  const timeWeightedLPAmount = lastLPAmountSaved.mul(timeDiff)
 
   if (lastLPAmountSaved.sub(log.amount).lt(0)) {
-    throw("user burned more than they had")
+    throw "user burned more than they had"
   }
 
   holders[log.address_from] = {
     lastLPAmountSaved: lastLPAmountSaved.sub(log.amount),
     lastActionTimestamp: currentTimestamp,
     firstObservedTimestamp: holder.firstObservedTimestamp,
-    timeWightedLPAmount: holder.timeWightedLPAmount.add(timeWeightedLPAmount)
+    timeWightedLPAmount: holder.timeWightedLPAmount.add(timeWeightedLPAmount),
   }
 }
 
-function processTimeWeightedAmount(allHolders: AllHolders): TimeWeightedAmountOutput {
-  let output: TimeWeightedAmountOutput = {}
+function processTimeWeightedAmount(
+  allHolders: AllHolders,
+): TimeWeightedAmountOutput {
+  const output: TimeWeightedAmountOutput = {}
 
-  let currentTimestamp = Math.round(new Date().getTime() / 1000)
+  const currentTimestamp = Math.round(new Date().getTime() / 1000)
   let nonUniqueLPCount = 0
 
-  for (let pool in allHolders) {
+  for (const pool in allHolders) {
     output[pool] = {}
-    let holders = allHolders[pool]
-    for (let address in holders) {
-      let holder = holders[address]
-      let timeDiff = currentTimestamp - holder.lastActionTimestamp
-      let timeWeightedLPAmount = holder.lastLPAmountSaved.mul(timeDiff)
-      holders[address].timeWightedLPAmount = holder.timeWightedLPAmount.add(timeWeightedLPAmount)
+    const holders = allHolders[pool]
+    for (const address in holders) {
+      const holder = holders[address]
+      const timeDiff = currentTimestamp - holder.lastActionTimestamp
+      const timeWeightedLPAmount = holder.lastLPAmountSaved.mul(timeDiff)
+      holders[address].timeWightedLPAmount =
+        holder.timeWightedLPAmount.add(timeWeightedLPAmount)
       output[pool][address] = holders[address].timeWightedLPAmount.toString()
       nonUniqueLPCount++
     }
@@ -110,23 +118,25 @@ function processTimeWeightedAmount(allHolders: AllHolders): TimeWeightedAmountOu
         const bigB = BigNumber.from(b)
         return bigA.lt(bigB) ? 1 : bigA.gt(bigB) ? -1 : 0
       })
-      .reduce((r, [k, v]) => ({...r, [k]: v}), {})
+      .reduce((r, [k, v]) => ({ ...r, [k]: v }), {})
   }
   console.log(`Non-unique LP count: ${nonUniqueLPCount}`)
   return output
 }
 
-function groupByAddress(timeWeightedAmounts: TimeWeightedAmountOutput): TimeWeightedAmountOutputByAddress {
-  let output: TimeWeightedAmountOutputByAddress = {}
+function groupByAddress(
+  timeWeightedAmounts: TimeWeightedAmountOutput,
+): TimeWeightedAmountOutputByAddress {
+  const output: TimeWeightedAmountOutputByAddress = {}
   let uniqueLPCount = 0
 
-  for (let pool in timeWeightedAmounts) {
-    for (let address in timeWeightedAmounts[pool]) {
+  for (const pool in timeWeightedAmounts) {
+    for (const address in timeWeightedAmounts[pool]) {
       if (output.hasOwnProperty(address)) {
         output[address][pool] = timeWeightedAmounts[pool][address]
       } else {
         output[address] = {
-          [pool] : timeWeightedAmounts[pool][address]
+          [pool]: timeWeightedAmounts[pool][address],
         }
         uniqueLPCount++
       }
@@ -137,23 +147,20 @@ function groupByAddress(timeWeightedAmounts: TimeWeightedAmountOutput): TimeWeig
 }
 
 function prettyStringfy(json: any) {
-  return JSON.stringify(
-    json,
-    null,
-    '    ')
+  return JSON.stringify(json, null, "    ")
 }
 
 function processAllLogs() {
-  let allHolders: AllHolders = {
+  const allHolders: AllHolders = {
     BTC: {},
     USD: {},
     vETH2: {},
     alETH: {},
-    d4: {}
+    d4: {},
   }
 
-  for (let log of lpTransferLogs) {
-    let holders = allHolders[log.pool]
+  for (const log of lpTransferLogs) {
+    const holders = allHolders[log.pool]
 
     // Minting
     if (log.address_from == "0x0000000000000000000000000000000000000000") {
@@ -174,15 +181,35 @@ function processAllLogs() {
   }
 
   // Write the allHolders object as JSON
-  fs.writeFile("./json/retroactive_lp_timeweighted_detailed.json", prettyStringfy(allHolders), 'utf8' ,() => {})
+  fs.writeFile(
+    "./json/retroactive_lp_timeweighted_detailed.json",
+    prettyStringfy(allHolders),
+    "utf8",
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    () => {},
+  )
 
   // Clean up allHolders objects to have only timeweighted amounts
   const timeWeightedAmountOutput = processTimeWeightedAmount(allHolders)
-  fs.writeFile("./json/retroactive_lp_timeweighted.json", prettyStringfy(timeWeightedAmountOutput), 'utf8' ,() => {})
+  fs.writeFile(
+    "./json/retroactive_lp_timeweighted.json",
+    prettyStringfy(timeWeightedAmountOutput),
+    "utf8",
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    () => {},
+  )
 
   // Group by addresses
-  const timeWeightedAmountOutputGroupedByAddress = groupByAddress(timeWeightedAmountOutput)
-  fs.writeFile("./json/retroactive_lp_timeweighted_by_address.json", prettyStringfy(timeWeightedAmountOutputGroupedByAddress), 'utf8' ,() => {})
+  const timeWeightedAmountOutputGroupedByAddress = groupByAddress(
+    timeWeightedAmountOutput,
+  )
+  fs.writeFile(
+    "./json/retroactive_lp_timeweighted_by_address.json",
+    prettyStringfy(timeWeightedAmountOutputGroupedByAddress),
+    "utf8",
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    () => {},
+  )
 }
 
 processAllLogs()
--- lp.ts
+++ lp.ts
@@ -1,9 +1,9 @@
-import * as data from './json/retroactive_lp.json'
+import * as data from "./json/retroactive_lp.json"
 
-import {BigNumber, ethers} from 'ethers';
+import { BigNumber, ethers } from "ethers"
 
-import fs from 'fs'
-import readline from 'readline'
+import fs from "fs"
+import readline from "readline"
 
 // Minute granularity BTC and ETH price data is downloaded from: https://duneanalytics.com/queries/95400
 
@@ -14,27 +14,30 @@ const END_BLOCK = 12923115
 const AVERAGE_BLOCK_TIME = 13 // used to estimate the timestamps for blocks we do not have logs for
 const TOTAL_LP_TOKENS = 120_000_000
 const TOTAL_BLOCKS = END_BLOCK - START_BLOCK
-const TOKENS_PER_BLOCK = ethers.utils.parseUnits(String(TOTAL_LP_TOKENS / TOTAL_BLOCKS), 18)
+const TOKENS_PER_BLOCK = ethers.utils.parseUnits(
+  String(TOTAL_LP_TOKENS / TOTAL_BLOCKS),
+  18,
+)
 
-let lpTransferLogs: LpTransferLog[] = data["default"]
+const lpTransferLogs: LpTransferLog[] = data["default"]
 
 interface LpTransferLog {
-  block_number: number,
-  block_timestamp: string,
-  transaction_hash: string,
-  address_from: string,
-  address_to: string,
-  amount: string,
+  block_number: number
+  block_timestamp: string
+  transaction_hash: string
+  address_from: string
+  address_to: string
+  amount: string
   pool: string
 }
 
 interface Holders {
   [holder: string]: {
-    lastLPAmountSaved: BigNumber;
-    lastActionTimestamp: number;
-    firstObservedTimestamp: number;
-    timeWightedLPAmount: BigNumber;
-  };
+    lastLPAmountSaved: BigNumber
+    lastActionTimestamp: number
+    firstObservedTimestamp: number
+    timeWightedLPAmount: BigNumber
+  }
 }
 
 interface AllHolders {
@@ -42,45 +45,51 @@ interface AllHolders {
 }
 
 interface TotalPoolLPTokens {
-  [pool: string]: BigNumber;
+  [pool: string]: BigNumber
 }
 
 interface TimeWeightedAmountOutput {
-  [pool: string] : {
-    [address: string] : string
+  [pool: string]: {
+    [address: string]: string
   }
 }
 
 interface TimeWeightedAmountOutputByAddress {
-  [address: string] : {
-    [pool: string] : string
+  [address: string]: {
+    [pool: string]: string
   }
 }
 
 interface MinutePriceData {
-  BTC: string,
+  BTC: string
   ETH: string
 }
 
 interface PriceData {
-  [timestamp: number] : MinutePriceData
+  [timestamp: number]: MinutePriceData
 }
 
-function processMinting(holders: Holders, tokens: TotalPoolLPTokens, log: LpTransferLog) {
-  let currentTimestamp = Math.round(new Date(log.block_timestamp).getTime() / 1000)
+function processMinting(
+  holders: Holders,
+  tokens: TotalPoolLPTokens,
+  log: LpTransferLog,
+) {
+  const currentTimestamp = Math.round(
+    new Date(log.block_timestamp).getTime() / 1000,
+  )
 
   tokens[log.pool] = tokens[log.pool].add(log.amount)
   if (holders.hasOwnProperty(log.address_to)) {
-    let holder = holders[log.address_to]
-    let lastLPAmountSaved = holder.lastLPAmountSaved
-    let timeDiff = currentTimestamp - holder.lastActionTimestamp
-    let timeWeightedLPAmount = lastLPAmountSaved.mul(timeDiff)
+    const holder = holders[log.address_to]
+    const lastLPAmountSaved = holder.lastLPAmountSaved
+    const timeDiff = currentTimestamp - holder.lastActionTimestamp
+    const timeWeightedLPAmount = lastLPAmountSaved.mul(timeDiff)
 
     holders[log.address_to] = {
       lastLPAmountSaved: lastLPAmountSaved.add(log.amount),
       lastActionTimestamp: currentTimestamp,
       firstObservedTimestamp: holder.firstObservedTimestamp,
-      timeWightedLPAmount: holder.timeWightedLPAmount.add(timeWeightedLPAmount)
+      timeWightedLPAmount: holder.timeWightedLPAmount.add(timeWeightedLPAmount),
     }
   } else {
     // No record for log.address_to found.
@@ -89,21 +98,27 @@ function processMinting(holders: Holders, tokens: TotalPoolLPTokens, log: LpTran
       lastLPAmountSaved: BigNumber.from(log.amount),
       lastActionTimestamp: currentTimestamp,
       firstObservedTimestamp: currentTimestamp,
-      timeWightedLPAmount: BigNumber.from(0)
+      timeWightedLPAmount: BigNumber.from(0),
     }
   }
 }
 
-function processBurning(holders: Holders, tokens: TotalPoolLPTokens, log: LpTransferLog) {
-  let currentTimestamp = Math.round(new Date(log.block_timestamp).getTime() / 1000)
+function processBurning(
+  holders: Holders,
+  tokens: TotalPoolLPTokens,
+  log: LpTransferLog,
+) {
+  const currentTimestamp = Math.round(
+    new Date(log.block_timestamp).getTime() / 1000,
+  )
 
-  let holder = holders[log.address_from]
-  let lastLPAmountSaved = holder.lastLPAmountSaved
-  let timeDiff = currentTimestamp - holder.lastActionTimestamp
-  let timeWeightedLPAmount = lastLPAmountSaved.mul(timeDiff)
+  const holder = holders[log.address_from]
+  const lastLPAmountSaved = holder.lastLPAmountSaved
+  const timeDiff = currentTimestamp - holder.lastActionTimestamp
+  const timeWeightedLPAmount = lastLPAmountSaved.mul(timeDiff)
 
   if (lastLPAmountSaved.sub(log.amount).lt(0)) {
-    throw(`user burned (${log.amount}) more than they had (${lastLPAmountSaved})`)
+    throw `user burned (${log.amount}) more than they had (${lastLPAmountSaved})`
   }
 
   tokens[log.pool] = tokens[log.pool].sub(log.amount)
@@ -111,24 +126,27 @@ function processBurning(holders: Holders, tokens: TotalPoolLPTokens, log: LpTran
     lastLPAmountSaved: lastLPAmountSaved.sub(log.amount),
     lastActionTimestamp: currentTimestamp,
     firstObservedTimestamp: holder.firstObservedTimestamp,
-    timeWightedLPAmount: holder.timeWightedLPAmount.add(timeWeightedLPAmount)
+    timeWightedLPAmount: holder.timeWightedLPAmount.add(timeWeightedLPAmount),
   }
 }
 
-function processTimeWeightedAmount(allHolders: AllHolders): TimeWeightedAmountOutput {
-  let output: TimeWeightedAmountOutput = {}
+function processTimeWeightedAmount(
+  allHolders: AllHolders,
+): TimeWeightedAmountOutput {
+  const output: TimeWeightedAmountOutput = {}
 
-  let currentTimestamp = Math.round(new Date().getTime() / 1000)
+  const currentTimestamp = Math.round(new Date().getTime() / 1000)
   let nonUniqueLPCount = 0
 
-  for (let pool in allHolders) {
+  for (const pool in allHolders) {
     output[pool] = {}
-    let holders = allHolders[pool]
-    for (let address in holders) {
-      let holder = holders[address]
-      let timeDiff = currentTimestamp - holder.lastActionTimestamp
-      let timeWeightedLPAmount = holder.lastLPAmountSaved.mul(timeDiff)
-      holders[address].timeWightedLPAmount = holder.timeWightedLPAmount.add(timeWeightedLPAmount)
+    const holders = allHolders[pool]
+    for (const address in holders) {
+      const holder = holders[address]
+      const timeDiff = currentTimestamp - holder.lastActionTimestamp
+      const timeWeightedLPAmount = holder.lastLPAmountSaved.mul(timeDiff)
+      holders[address].timeWightedLPAmount =
+        holder.timeWightedLPAmount.add(timeWeightedLPAmount)
       output[pool][address] = holders[address].timeWightedLPAmount.toString()
       nonUniqueLPCount++
     }
@@ -140,23 +158,25 @@ function processTimeWeightedAmount(allHolders: AllHolders): TimeWeightedAmountOu
         const bigB = BigNumber.from(b)
         return bigA.lt(bigB) ? 1 : bigA.gt(bigB) ? -1 : 0
       })
-      .reduce((r, [k, v]) => ({...r, [k]: v}), {})
+      .reduce((r, [k, v]) => ({ ...r, [k]: v }), {})
   }
   console.log(`Non-unique LP count: ${nonUniqueLPCount}`)
   return output
 }
 
-function groupByAddress(timeWeightedAmounts: TimeWeightedAmountOutput): TimeWeightedAmountOutputByAddress {
-  let output: TimeWeightedAmountOutputByAddress = {}
+function groupByAddress(
+  timeWeightedAmounts: TimeWeightedAmountOutput,
+): TimeWeightedAmountOutputByAddress {
+  const output: TimeWeightedAmountOutputByAddress = {}
   let uniqueLPCount = 0
 
-  for (let pool in timeWeightedAmounts) {
-    for (let address in timeWeightedAmounts[pool]) {
+  for (const pool in timeWeightedAmounts) {
+    for (const address in timeWeightedAmounts[pool]) {
       if (output.hasOwnProperty(address)) {
         output[address][pool] = timeWeightedAmounts[pool][address]
       } else {
         output[address] = {
-          [pool] : timeWeightedAmounts[pool][address]
+          [pool]: timeWeightedAmounts[pool][address],
         }
         uniqueLPCount++
       }
@@ -167,10 +187,7 @@ function groupByAddress(timeWeightedAmounts: TimeWeightedAmountOutput): TimeWeig
 }
 
 function prettyStringfy(json: any) {
-  return JSON.stringify(
-    json,
-    null,
-    '    ')
+  return JSON.stringify(json, null, "    ")
 }
 
 async function loadPriceData(): Promise<PriceData> {
@@ -181,14 +198,14 @@ async function loadPriceData(): Promise<PriceData> {
   })
 
   for await (const line of rl) {
-    let [tsRaw, price, asset] = line.split(',')
+    const [tsRaw, price, asset] = line.split(",")
 
-    let ts = Number(tsRaw)
+    const ts = Number(tsRaw)
 
     if (!data[ts]) {
       data[ts] = {
         BTC: "",
-        ETH: ""
+        ETH: "",
       }
     }
 
@@ -203,28 +220,31 @@ async function loadPriceData(): Promise<PriceData> {
 }
 
 async function processAllLogs() {
-  let allHolders: AllHolders = {
+  const allHolders: AllHolders = {
     BTC: {},
     USD: {},
     vETH2: {},
     alETH: {},
-    d4: {}
+    d4: {},
   }
-  let lpTokens: TotalPoolLPTokens = {
+  const lpTokens: TotalPoolLPTokens = {
     BTC: BigNumber.from(0),
     USD: BigNumber.from(0),
     vETH2: BigNumber.from(0),
     alETH: BigNumber.from(0),
-    d4: BigNumber.from(0)
+    d4: BigNumber.from(0),
   }
-  let rewards: { [address: string]: BigNumber} = {}
+  const rewards: { [address: string]: BigNumber } = {}
 
   // Load price data
   const priceData = await loadPriceData()
 
   // Pre-process logs to group them by block
-  let logsByBlock: Map<number, LpTransferLog[]> = new Map<number, LpTransferLog[]>()
-  for (let log of lpTransferLogs) {
+  const logsByBlock: Map<number, LpTransferLog[]> = new Map<
+    number,
+    LpTransferLog[]
+  >()
+  for (const log of lpTransferLogs) {
     const { block_number: raw } = log
     // TODO: why does this not work without explicitly casting?
     const block_number = Number(raw)
@@ -250,38 +270,53 @@ async function processAllLogs() {
       const blocks = logsByBlock.get(block)
       // Parse and truncate the timestamp to minute level precision for retrieving pricing data
       const parsed = Date.parse(blocks[0].block_timestamp) / 1000
-      ts = parsed - parsed % 60
+      ts = parsed - (parsed % 60)
 
       // Filter blocks by transaction type for processing
-      const minting = blocks.filter(log => log.address_from === "0x0000000000000000000000000000000000000000")
-      const burning = blocks.filter(log => log.address_to === "0x0000000000000000000000000000000000000000")
-      const transfers = blocks.filter(log => (log.address_from !== "0x0000000000000000000000000000000000000000" && log.address_to !== "0x0000000000000000000000000000000000000000"))
+      const minting = blocks.filter(
+        (log) =>
+          log.address_from === "0x0000000000000000000000000000000000000000",
+      )
+      const burning = blocks.filter(
+        (log) =>
+          log.address_to === "0x0000000000000000000000000000000000000000",
+      )
+      const transfers = blocks.filter(
+        (log) =>
+          log.address_from !== "0x0000000000000000000000000000000000000000" &&
+          log.address_to !== "0x0000000000000000000000000000000000000000",
+      )
 
       // Process minting before burning
-      for (let log of [...minting, ...transfers]) {
-        let holders = allHolders[log.pool];
-        processMinting(holders, lpTokens, log);
-        allHolders[log.pool] = holders;
+      for (const log of [...minting, ...transfers]) {
+        const holders = allHolders[log.pool]
+        processMinting(holders, lpTokens, log)
+        allHolders[log.pool] = holders
       }
 
-      for (let log of [...burning, ...transfers]) {
-        let holders = allHolders[log.pool];
-        processBurning(holders, lpTokens, log);
-        allHolders[log.pool] = holders;
+      for (const log of [...burning, ...transfers]) {
+        const holders = allHolders[log.pool]
+        processBurning(holders, lpTokens, log)
+        allHolders[log.pool] = holders
       }
     } else {
       // If there were no logs for a particular block we don't have the unix timestamp, so estimate it
-      const estimate = START_BLOCK_TS + ((block - START_BLOCK) * AVERAGE_BLOCK_TIME)
-      ts = estimate - estimate % 60
+      const estimate =
+        START_BLOCK_TS + (block - START_BLOCK) * AVERAGE_BLOCK_TIME
+      ts = estimate - (estimate % 60)
     }
 
     // Calculate total pool USD TVL
     let totalUSDTVL = BigNumber.from(0)
-    for (let pool in allHolders) {
+    for (const pool in allHolders) {
       if (pool === "BTC") {
-        totalUSDTVL = totalUSDTVL.add(lpTokens[pool].mul(ethers.utils.parseUnits(priceData[ts].BTC, 2)))
+        totalUSDTVL = totalUSDTVL.add(
+          lpTokens[pool].mul(ethers.utils.parseUnits(priceData[ts].BTC, 2)),
+        )
       } else if (pool === "vETH2" || pool === "alETH") {
-        totalUSDTVL = totalUSDTVL.add(lpTokens[pool].mul(ethers.utils.parseUnits(priceData[ts].ETH, 2)))
+        totalUSDTVL = totalUSDTVL.add(
+          lpTokens[pool].mul(ethers.utils.parseUnits(priceData[ts].ETH, 2)),
+        )
       } else {
         totalUSDTVL = totalUSDTVL.add(lpTokens[pool])
       }
@@ -290,17 +325,29 @@ async function processAllLogs() {
     // Distribute tokens
     // TODO: Double the rewards issuance during the guarded launch period
     // TODO: Parallelize with promises for a performance improvement?
-    for (let pool in allHolders) {
-      let holders = allHolders[pool]
-      for (let address in holders) {
-        let userShare = holders[address].lastLPAmountSaved
+    for (const pool in allHolders) {
+      const holders = allHolders[pool]
+      for (const address in holders) {
+        const userShare = holders[address].lastLPAmountSaved
         if (!rewards[address]) rewards[address] = BigNumber.from(0)
         if (pool === "BTC") {
-          rewards[address] = rewards[address].add(userShare.mul(ethers.utils.parseUnits(priceData[ts].BTC, 2)).mul(TOKENS_PER_BLOCK).div(totalUSDTVL))
+          rewards[address] = rewards[address].add(
+            userShare
+              .mul(ethers.utils.parseUnits(priceData[ts].BTC, 2))
+              .mul(TOKENS_PER_BLOCK)
+              .div(totalUSDTVL),
+          )
         } else if (pool === "vETH2" || pool === "alETH") {
-          rewards[address] = rewards[address].add(userShare.mul(ethers.utils.parseUnits(priceData[ts].ETH, 2)).mul(TOKENS_PER_BLOCK).div(totalUSDTVL))
+          rewards[address] = rewards[address].add(
+            userShare
+              .mul(ethers.utils.parseUnits(priceData[ts].ETH, 2))
+              .mul(TOKENS_PER_BLOCK)
+              .div(totalUSDTVL),
+          )
         } else {
-          rewards[address] = rewards[address].add(userShare.mul(TOKENS_PER_BLOCK).div(totalUSDTVL))
+          rewards[address] = rewards[address].add(
+            userShare.mul(TOKENS_PER_BLOCK).div(totalUSDTVL),
+          )
         }
       }
     }
@@ -308,22 +355,43 @@ async function processAllLogs() {
 
   // Sanity check the distribution
   let totalRewardsDistributed = BigNumber.from(0)
-  for (const [_, reward] of Object.entries(rewards)) {
+  for (const [, reward] of Object.entries(rewards)) {
     totalRewardsDistributed = totalRewardsDistributed.add(reward)
   }
 
-  console.assert(ethers.utils.formatUnits(totalRewardsDistributed, 18) === String(TOTAL_LP_TOKENS), `rewards did not match (got, expected): ${ethers.utils.formatUnits(totalRewardsDistributed, 18) }, ${TOTAL_LP_TOKENS}`)
+  console.assert(
+    ethers.utils.formatUnits(totalRewardsDistributed, 18) ===
+      String(TOTAL_LP_TOKENS),
+    `rewards did not match (got, expected): ${ethers.utils.formatUnits(
+      totalRewardsDistributed,
+      18,
+    )}, ${TOTAL_LP_TOKENS}`,
+  )
 
   // Write the allHolders object as JSON
-  fs.writeFile("./json/retroactive_lp_timeweighted_detailed.json", prettyStringfy(allHolders), 'utf8' ,() => {})
+  await fs.promises.writeFile(
+    "./json/retroactive_lp_timeweighted_detailed.json",
+    prettyStringfy(allHolders),
+    "utf8",
+  )
 
   // Clean up allHolders objects to have only timeweighted amounts
   const timeWeightedAmountOutput = processTimeWeightedAmount(allHolders)
-  fs.writeFile("./json/retroactive_lp_timeweighted.json", prettyStringfy(timeWeightedAmountOutput), 'utf8' ,() => {})
+  await fs.promises.writeFile(
+    "./json/retroactive_lp_timeweighted.json",
+    prettyStringfy(timeWeightedAmountOutput),
+    "utf8",
+  )
 
   // Group by addresses
-  const timeWeightedAmountOutputGroupedByAddress = groupByAddress(timeWeightedAmountOutput)
-  fs.writeFile("./json/retroactive_lp_timeweighted_by_address.json", prettyStringfy(timeWeightedAmountOutputGroupedByAddress), 'utf8' ,() => {})
+  const timeWeightedAmountOutputGroupedByAddress = groupByAddress(
+    timeWeightedAmountOutput,
+  )
+  await fs.promises.writeFile(
+    "./json/retroactive_lp_timeweighted_by_address.json",
+    prettyStringfy(timeWeightedAmountOutputGroupedByAddress),
+    "utf8",
+  )
 }
 
 processAllLogs()
