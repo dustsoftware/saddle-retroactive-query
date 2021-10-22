42851006507b3154e18210fd5c7c48fef6425b6a
Jongseung Lim <penandlim@gmail.com>
2021-10-22 14:58:23 +0100

    Replace addresses with lower cases

--- src/010_saddle_retroactive_swap.sql
+++ src/010_saddle_retroactive_swap.sql
@@ -31,11 +31,11 @@ BEGIN
                    address = '0xdec2157831d6abc3ec328291119cc91b337272b5' OR -- vETH2 pool
                    address = '0xa6018520eaacc06c30ff2e1b3ee2c7c22e64196a' OR -- alETH pool
                    address = '0xc69ddcd4dfef25d8a793241834d4cc4b3668ead6' OR -- d4 pool
-                   address = '0xaCb83E0633d6605c5001e2Ab59EF3C745547C8C7' OR -- USDv2 pool
-                   address = '0x0C8BAe14c9f9BF2c953997C881BEfaC7729FD314' OR -- sUSD metapool
-                   address = '0xdf3309771d2BF82cb2B6C56F9f5365C8bD97c4f2' OR -- BTCv2 pool
-                   address = '0xf74ebe6e5586275dc4CeD78F5DBEF31B1EfbE7a5' OR -- tBTCv2 metapool
-                   address = '0x3F1d224557afA4365155ea77cE4BC32D5Dae2174')             -- WCUSD metapool
+                   address = '0xacb83e0633d6605c5001e2ab59ef3c745547c8c7' OR -- USDv2 pool
+                   address = '0x0c8bae14c9f9bf2c953997c881befac7729fd314' OR -- sUSD metapool
+                   address = '0xdf3309771d2bf82cb2b6c56f9f5365c8bd97c4f2' OR -- BTCv2 pool
+                   address = '0xf74ebe6e5586275dc4ced78f5dbef31b1efbe7a5' OR -- tBTCv2 metapool
+                   address = '0x3f1d224557afa4365155ea77ce4bc32d5dae2174')             -- WCUSD metapool
               AND topics[SAFE_OFFSET(0)] =
                   '0xc6c1e0630dbe9130cc068028486c0d118ddcea348550819defd5cb8c257f8a38' -- "TokenSwap" event
               AND block_number <= 13330090 -- 2021-10-01 00:00:00
--- src/020_saddle_retroactive_lp.sql
+++ src/020_saddle_retroactive_lp.sql
@@ -21,21 +21,21 @@ BEGIN
         (
             SELECT logs.address                                                  as token,
                    logs.block_number                                             as block_number,
-                   logs.block_timestamp                                          as block_timestamp,
                    PARSE_SADDLE_LP_TRANSFER_EVENTS(logs.data, logs.topics).from  as address_from,
                    PARSE_SADDLE_LP_TRANSFER_EVENTS(logs.data, logs.topics).to    as address_to,
                    PARSE_SADDLE_LP_TRANSFER_EVENTS(logs.data, logs.topics).value as amount
+
             FROM `bigquery-public-data.crypto_ethereum.logs` AS logs
-            WHERE (address = '0xC28DF698475dEC994BE00C9C9D8658A548e6304F' OR -- BTC lp token
-                   address = '0x76204f8CFE8B95191A3d1CfA59E267EA65e06FAC' OR -- USD lp token
-                   address = '0xe37E2a01feA778BC1717d72Bd9f018B6A6B241D5' OR -- vETH2 lp token
-                   address = '0xc9da65931ABf0Ed1b74Ce5ad8c041C4220940368' OR -- alETH lp token
-                   address = '0xd48cF4D7FB0824CC8bAe055dF3092584d0a1726A' OR -- d4 lp token
-                   address = '0x5f86558387293b6009d7896A61fcc86C17808D62' OR -- USD v2 lp token
-                   address = '0x8Fa31c1b33De16bf05c38AF20329f22D544aD64c' OR -- sUSD metapool lp token
-                   address = '0xF32E91464ca18fc156aB97a697D6f8ae66Cd21a3' OR -- BTC v2 lp token
-                   address = '0x122Eca07139EB368245A29FB702c9ff11E9693B7' OR -- tBTC metapool lp token
-                   address = '0x78179d49C13c4ECa14C69545ec172Ba0179EAE6B')             -- wCUSD metapool lp token
+            WHERE (address = '0xc28df698475dec994be00c9c9d8658a548e6304f' OR -- BTC lp token
+                   address = '0x76204f8cfe8b95191a3d1cfa59e267ea65e06fac' OR -- USD lp token
+                   address = '0xe37e2a01fea778bc1717d72bd9f018b6a6b241d5' OR -- vETH2 lp token
+                   address = '0xc9da65931abf0ed1b74ce5ad8c041c4220940368' OR -- alETH lp token
+                   address = '0xd48cf4d7fb0824cc8bae055df3092584d0a1726a' OR -- d4 lp token
+                   address = '0xd48cf4d7fb0824cc8bae055df3092584d0a1726a' OR -- USD v2 lp token
+                   address = '0x8fa31c1b33de16bf05c38af20329f22d544ad64c' OR -- sUSD metapool lp token
+                   address = '0xf32e91464ca18fc156ab97a697d6f8ae66cd21a3' OR -- BTC v2 lp token
+                   address = '0x122eca07139eb368245a29fb702c9ff11e9693b7' OR -- tBTC metapool lp token
+                   address = '0x78179d49c13c4eca14c69545ec172ba0179eae6b')   -- wCUSD metapool lp token
               AND topics[SAFE_OFFSET(0)] =
                   '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' -- "Transfer" event
               AND block_number <= 13330090 -- 2021-10-01 00:00:00
